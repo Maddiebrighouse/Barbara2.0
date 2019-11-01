1ed0636f3c22c9050c38823738f7de7ad98b8ccb
Madeleine Brighouse <Madeleinebrighouse@gmail.com>
2019-11-01 14:43:11 +1300

    responsive css

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -19,23 +19,21 @@ const Contact = () => {
       </div>
       <hr className={styles.hr1} />
       <div className={styles.contactdetails}>
-          <h3 className={styles}>
-            Barbara Darragh 
-          </h3>
-          <a className={styles.a} href="mailto:barbara.darragh@gmail.com">barbara.darragh@gmail.com</a>
-          <p className={styles.text}>
-            6421948255 
-          </p>
-          <h3 className={styles}>
-              Gems Agency UK 
-          </h3>
-          <a className={styles.a} href="mailto:Jayne@gems-agency.co.uk">Jayne@gems-agency.co.uk</a>
-          <hr className={styles.hr2} />
-          <img
-            className={styles.needle2}
-            src="https://res.cloudinary.com/madeleinebrighouse/image/upload/v1572477395/Barbara%27s%20Website/needle_zcgofg.svg"
-            alt="Drawing of a needle"
-          />
+        <h3 className={styles}>Barbara Darragh</h3>
+        <a className={styles.a} href="mailto:barbara.darragh@gmail.com">
+          barbara.darragh@gmail.com
+        </a>
+        <p className={styles.text}>6421948255</p>
+        <h3 className={styles}>Gems Agency UK</h3>
+        <a className={styles.a} href="mailto:Jayne@gems-agency.co.uk">
+          Jayne@gems-agency.co.uk
+        </a>
+        <hr className={styles.hr2} />
+        <img
+          className={styles.needle2}
+          src="https://res.cloudinary.com/madeleinebrighouse/image/upload/v1572477395/Barbara%27s%20Website/needle_zcgofg.svg"
+          alt="Drawing of a needle"
+        />
       </div>
     </div>
   );
