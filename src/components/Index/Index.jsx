2ea8b34bdc190cfbaffbf79bc5ffe426296b01c3
Madeleine Brighouse <Madeleinebrighouse@gmail.com>
2019-11-04 15:52:07 +1300

    imported and implomented Slider-React, TODO Css changes and Resopnsiveness

--- src/components/Index/Index.jsx
+++ src/components/Index/Index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import request from 'superagent';
+import Moment from 'react-moment'
+import Slider from 'react-slick'
 
 import styles from './index.css';
 
@@ -21,25 +23,37 @@ class Index extends React.Component {
   }
 
   render() {
+    var settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 6,
+        slidesToScroll: 4,
+        initialSlide: 0,
+      };
+    
     return (
-      this.state.work.map(work => {
-        return(
-            <div className={styles.item}>
-                <img
-                    className={styles.image}
-                    src={work.image}
-                    alt=""
-                ></img>
-                <div className={styles.middle}>
-                    <div className={styles.text}>
-                    <p>{work.name}</p>
-                    <p>{work.date}</p>
-                    <p>{work.occupation}</p>
+        <Slider {...settings}>{this.state.work.map(work => {
+            return(
+                <a href={work.link} key={work._id} target="_blank">
+                    <div className={styles.item}>
+                        <img
+                            className={styles.image}
+                            src={work.image}
+                            alt=""
+                        ></img>
+                        <div className={styles.overlay}>
+                            <div className={styles.text}>
+                            <h1>{work.name}</h1>
+                            <h3 className={styles.category}>{work.category}</h3>
+                            <Moment format="YYYY" withTitle>{work.date}</Moment> 
+                            <p>{work.occupation}</p>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-        )
-      }) 
+                </a>
+            )
+          }) }</Slider>
     );
   }
 }
