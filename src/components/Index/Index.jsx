033caa63307af9d650103c7ae13255eba921e1fa
Madeleine Brighouse <Madeleinebrighouse@gmail.com>
2019-11-06 22:54:12 +1300

    Made Slider responsive Css

--- src/components/Index/Index.jsx
+++ src/components/Index/Index.jsx
@@ -27,9 +27,43 @@ class Index extends React.Component {
         dots: true,
         infinite: true,
         speed: 500,
-        slidesToShow: 6,
-        slidesToScroll: 4,
+        slidesToShow: 5,
+        slidesToScroll: 5,
         initialSlide: 0,
+        responsive: [
+            {
+              breakpoint: 1400,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 4,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                initialSlide: 2
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              } 
+            },
+            {
+                breakpoint: 414,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                  dots: false
+                } 
+              }
+          ]
       };
     
     return (
