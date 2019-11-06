7e922d7a26aac09a4c6363b7a0effb9cae7c6906
Madeleine Brighouse <Madeleinebrighouse@gmail.com>
2019-11-07 11:49:02 +1300

    Replaced Title Url with correct one, added Alt tags

--- src/components/Homepage/homepage.jsx
+++ src/components/Homepage/homepage.jsx
@@ -6,13 +6,13 @@ const Homepage = () => {
     <div className={styles.homepage}>
       <img
         className={styles.mainImage}
-        src="https://res.cloudinary.com/madeleinebrighouse/image/upload/v1572477393/Barbara%27s%20Website/barbara_qnwrgk.svg"
-        alt="Sketch of Bargbara"
+        src="https://res.cloudinary.com/madeleinebrighouse/image/upload/v1573080094/Barbara%27s%20Website/Barbara-Darragh-Title_bgcgdz.svg"
+        alt="Sketch/Link drawing of Barbara Darragh, like a sribble outlink of her face"
       />
       <img
         className={styles.mainTitle}
         src="https://res.cloudinary.com/madeleinebrighouse/image/upload/v1572477393/Barbara%27s%20Website/Barbara_title_gbs5x8.svg"
-        alt="Title Barbara Darragh"
+        alt="Hand written title saying Barbara Darragh"
       />
       <div className={styles.lucy}>
         <p className={styles.text}>Image inspired by Lucy Danner</p>
