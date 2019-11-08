e41211df72388e892e5ca4713521b16b1d16d565
Madeleine Brighouse <Madeleinebrighouse@gmail.com>
2019-11-08 15:42:57 +1300

    Fix url to correct Images

--- src/components/Homepage/homepage.jsx
+++ src/components/Homepage/homepage.jsx
@@ -6,12 +6,12 @@ const Homepage = () => {
     <div className={styles.homepage}>
       <img
         className={styles.mainImage}
-        src="https://res.cloudinary.com/madeleinebrighouse/image/upload/v1573080094/Barbara%27s%20Website/Barbara-Darragh-Title_bgcgdz.svg"
+        src="https://res.cloudinary.com/madeleinebrighouse/image/upload/v1572477393/Barbara%27s%20Website/barbara_qnwrgk.svg"
         alt="Sketch/Link drawing of Barbara Darragh, like a sribble outlink of her face"
       />
       <img
         className={styles.mainTitle}
-        src="https://res.cloudinary.com/madeleinebrighouse/image/upload/v1572477393/Barbara%27s%20Website/Barbara_title_gbs5x8.svg"
+        src="https://res.cloudinary.com/madeleinebrighouse/image/upload/v1573080094/Barbara%27s%20Website/Barbara-Darragh-Title_bgcgdz.svg"
         alt="Hand written title saying Barbara Darragh"
       />
       <div className={styles.lucy}>
